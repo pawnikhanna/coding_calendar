4df683c4508688745400476d3a4245757a076708
pawnikhanna <51890749+pawnikhanna@users.noreply.github.com>
2020-08-01 23:10:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+const axios = require('axios');
+const arg = process.argv[2];
+
+const getContests = async() => {
+  try{
+    return await axios.get('https://clist.by/get/events/')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const events = async() => {
+  const contests = await getContests()
+
+  if(contests.data) {
+    result = contests.data;
+    let currDate = new Date();
+
+    switch (arg) {
+      case "past" :
+        console.log ('PAST EVENTS');
+        result.forEach(function(elem) {
+          let start = elem.start;
+          start = new Date(start);
+          if (start < currDate) {
+            console.log(elem);
+        }
+        })
+        break;
+      case "ongoing" :
+        console.log ('ONGOING EVENTS');
+        result.forEach(function(elem) {
+          let start = elem.start;
+          start = new Date(start);
+          let end = elem.end;
+          end = new Date(end);
+          if (currDate < end && currDate >= start) {
+            console.log(elem);
+          }
+        })
+        break;
+      case "upcoming" :
+        console.log ('UPCOMING EVENTS');
+        result.forEach(function(elem) {
+          let start = elem.start;
+          start = new Date(start);
+          if (start > currDate) {
+            console.log(elem);
+          }
+        })
+        break;
+      default:
+        console.log('Wrong Choice');
+    }
+  }
+}
+events()
